69b99f05f9260726795288fe46b1564268dbfae8
renukrishnan <renukrishnan12@gmail.com>
2021-01-11 18:30:36 +0530

    dateofbirth commit

---
+++ languagefundamentals/dateofbirth.py
@@ -0,0 +1,5 @@
+name=input("enter name =")
+date=int(input(" enter date ="))
+month=int(input(" enter month ="))
+year=int(input(" enter year ="))
+print("The date of birth of",name,"is",date,"-",month,"-",year)
