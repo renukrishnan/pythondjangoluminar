1e50d89da6fa70b6e51febf9f19f54c80c136e52
renukrishnan <renukrishnan12@gmail.com>
2021-01-13 11:26:43 +0530

    adding workshop questions

---
+++ workshop_questions/occuringcharacters.py
@@ -0,0 +1,5 @@
+word=input("enter the string:")
+length=len(word)-1
+count=0
+if((word)
+
---
+++ workshop_questions/squareroot.py
@@ -0,0 +1,4 @@
+import math
+num=int(input("enter number:"))
+x=math.sqrt(num)
+print("the square root of",num,"is",x)
